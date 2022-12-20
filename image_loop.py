f1f86f7141769e2fa016846c1e2f997a8f4220dc
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2022-12-20 21:03:36 +0100

    Update image_loop.py
    Co-authored-by: Miriam Agrawala <100138928+miriam-agrawala@users.noreply.github.com>

--- image_loop.py
+++ image_loop.py
@@ -8,7 +8,7 @@ folder = "/Users/lizak/OneDrive/Desktop/myntradataset/images/"
 onlyfiles = []
 
 # Iterate over the files in the specified folder
-for f in os.listdir(folder):
+for filename in glob.glob('path/*.jpg'): #using * as a wildcard for the exact image name
     # Construct the full file path by joining the folder path with the file name
     file_path = os.path.join(folder, f)
     # Check if the file path is a file (and not a directory or symbolic link, etc.)
