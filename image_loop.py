488e228c0e47f398c99a27f0466a0bfa74576ca1
Liza Kaladjian <lizakalaijiann@gmail.com>
2022-12-20 15:50:34 +0100

    Just added the comments

--- image_loop.py
+++ image_loop.py
@@ -2,7 +2,11 @@ import os
 
 folder = "/Users/lizak/OneDrive/Desktop/myntradataset/images/"
 onlyfiles = []
+
+# Iterate over the files in the specified folder
 for f in os.listdir(folder):
+    # Construct the full file path by joining the folder path with the file name
     file_path = os.path.join(folder, f)
+    # Check if the file path is a file (and not a directory or symbolic link, etc.)
     if os.path.isfile(file_path):
         onlyfiles.append(f)
