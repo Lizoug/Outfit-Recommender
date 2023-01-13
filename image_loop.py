81a45225420495427d9b7291ae7f9d8b4010f767
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-01-13 16:04:27 +0100

    Update image_loop.py

--- image_loop.py
+++ image_loop.py
@@ -1,12 +1,11 @@
 import glob
 from PIL import Image
 
-folder = os.getcwd()
+path = os.getcwd()
 onlyfiles = []
 
 # Iterate over the files in the specified folder
 for filename in glob.glob('path/*.jpg'): #using * as a wildcard for the exact image name
-for filename in glob.glob('folder/*.jpg'): #using * as a wildcard for the exact image name
     # Check if the file path is a file (and not a directory or symbolic link, etc.)
    im = Image.open(filename) 
    onlyfiles.append(im)
