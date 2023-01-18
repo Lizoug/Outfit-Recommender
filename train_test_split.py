7690b01cf79964a2005eda925ed8941111c9d862
Miriam Agrawala <100138928+miriam-agrawala@users.noreply.github.com>
2023-01-18 12:10:58 +0100

    Update train_test_split.py
    Co-authored-by: Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>

--- train_test_split.py
+++ train_test_split.py
@@ -3,4 +3,5 @@
 X_train, X_test, y_train, y_test = train_test_split(images_to_numpy,
                                                     lables_one_hot_encoded_to_numpy,
                                                     test_size=0.2,
-                                                    random_state=0, shuffle=True)
+                                                    random_state=0, shuffle=True,
+                                                    metrics=['accuracy'])
