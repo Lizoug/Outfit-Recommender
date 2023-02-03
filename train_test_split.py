9954ab9b954e893582ba62fc6cfbee1bfec6aadf
bro-tec <khan0shazil@gmail.com>
2023-02-03 22:56:00 +0100

    deleting unnecessary stuff

--- train_test_split.py
+++
@@ -1,7 +0,0 @@
-
-#train-test-split
-X_train, X_test, y_train, y_test = train_test_split(images_to_numpy,
-                                                    lables_one_hot_encoded_to_numpy,
-                                                    test_size=0.2,
-                                                    random_state=0, shuffle=True,
-                                                    metrics=['accuracy'])
