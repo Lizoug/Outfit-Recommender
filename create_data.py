df70a6d20122b7e88697dcc2ae64d73d78a6fa7b
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-08 15:29:01 +0100

    imported libraries

---
+++ create_data.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+from tensorflow import keras
+import numpy as np
+import glob
+from PIL import Image
+import numpy as np 
+from sklearn.model_selection import train_test_split
