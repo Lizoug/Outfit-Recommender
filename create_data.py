58350d8b39547f55793143ec494a50a55569e436
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-08 15:31:24 +0100

    added convert_image_to_array_endlist(path_image, data)

--- create_data.py
+++ create_data.py
@@ -47,3 +47,41 @@ def train_test(path_image):
     X_train, X_test = X_train / 255.0, X_test / 255.0
     
     return X_train, X_test, y_train, y_test
+
+
+images = []
+lable_article = []
+def convert_image_to_array_endlist(path_image, data):
+    """ This function takes a single argument "path_image". 
+    Within the function, it uses the glob library to get 
+    all the jpg images in the given path, enumerates through them,
+    #extracts the image id from the filename, uses that id to find
+    the corresponding entry in a dataframe, opens the image using
+    the Image module and converts it to a numpy array. If the image
+    shape is (80,60,3) then it appends the image and the corresponding
+    article type to the 'images' and 'lable_article' lists respectively.
+    It returns the 'images' and 'lable_article' lists."""
+
+    
+    for i,filename in enumerate(glob.glob(f'{path_image}*.jpg')): 
+
+    	# Extract the image id from the filename
+        image_id = int(filename.split('\\')[-1].split('.')[0])
+
+        # Use the image id to find the corresponding entry in the 'data' dataframe
+        entry_of_dataframe_with_correct_imageid = data[data["image_id"] == image_id]
+    
+    	# If the entry is empty, continue to the next iteration
+        if entry_of_dataframe_with_correct_imageid.empty:
+                continue
+
+        # Open the image using the Image module and convert it to a numpy array
+        image = Image.open(filename)    
+        image = np.array(image)
+
+        if image.shape == (80,60,3): 
+            only_entry_of_column_articleType = entry_of_dataframe_with_correct_imageid.iloc[0,2]
+            lable_article.append(only_entry_of_column_articleType)
+            images.append(image)
+
+    return images, lable_article
