a1e351072be40b5fb1b93e3f350a337d5592c29b
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-08 15:29:54 +0100

    added create_dataset(path_csv)

--- create_data.py
+++ create_data.py
@@ -6,3 +6,25 @@ import glob
 from PIL import Image
 import numpy as np 
 from sklearn.model_selection import train_test_split
+
+def create_dataset(path_csv):
+    data = pd.read_csv(path_csv+"/styles.csv",error_bad_lines = False)
+    
+    
+    # Issue clean data
+    
+    data = data.drop(['year','usage', 'productDisplayName', 'masterCategory', 'subCategory'], axis=1)
+    
+    data = data.rename(columns={'id': 'image_id'})
+    
+    class_names = ['Shirts', 'Jeans', 'Track Pants', 'Tshirts', 'Casual Shoes', 'Tops',
+                   'Sandals', 'Sweatshirts', 'Formal Shoes', 'Waistcoat', 'Sports Shoes', 'Shorts', 
+                   'Heels', 'Innerwear Vests', 'Rain Jacket', 'Dresses', 'Skirts', 'Blazers',
+                   'Shrug', 'Trousers', 'Jackets', 'Sports Sandals', 'Sweaters', 'Tracksuits', 
+                   'Leggings', 'Jumpsuit', 'Robe', 'Salwar and Dupatta', 'Kurtas', 'Sarees']
+    
+    #delete rows that are not in class_names
+    data = data.loc[data["articleType"].isin(class_names)]
+    
+    data = data.dropna()
+    return data 
