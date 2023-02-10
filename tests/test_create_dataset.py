be39436ee2119d81f04e0927e49863d55d3bd504
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-10 23:24:53 +0100

    a new test function

--- tests/test_create_dataset.py
+++ tests/test_create_dataset.py
@@ -7,31 +7,22 @@ import create_data
 
 
 def test_create_dataset():
-    # Create a small sample of the dataset to use in the test
-    sample_data = {'image_id': [1, 2, 3, 4, 5],
-                   'articleType': ['Shirts', 'Jeans',
-                                   'Track Pants', 'Tshirts',
-                                   'Casual Shoes'],
-                   'baseColour': ['Navy Blue', 'Blue',
-                                  'Silver', 'Black', 'Grey']}
+    path_csv = "test.csv"
+    test_df = pd.DataFrame({'id': [1, 2, 3, 4], 
+                            'articleType': ['Shirts', 'Jeans', 'Tshirts', 'Tops'], 
+                            'baseColour': ['Navy Blue', 'Silver', 'Black', 'Green'], 
+                            'year': [2010, 2011, 2012, 2013], 
+                            'usage': ['Casual', 'Formal', 'Party', 'Sports'],
+                            'productDisplayName': ['Test1', 'Test2', 'Test3', 'Test4'], 
+                            'masterCategory': ['Clothing', 'Clothing', 'Clothing', 'Clothing'],
+                            'subCategory': ['Topwear', 'Bottomwear', 'Topwear', 'Topwear']})
+    test_df.to_csv(path_csv, index=False)
+    
+    result = create_data.create_dataset(path_csv)
+    expected_df = pd.DataFrame({'image_id': [1, 2, 3, 4], 
+                                'articleType': ['Shirts', 'Jeans', 'Tshirts', 'Tops'], 
+                                'baseColour': ['Navy Blue', 'Silver', 'Black', 'Green']})
+    assert result.equals(expected_df), f"Expected {expected_df} but got {result}"
+    
+    os.remove(path_csv)
 
-    sample_df = pd.DataFrame(sample_data)
-
-    # Pass the sample dataset to the create_dataset function
-    result = create_data.create_dataset(sample_df)
-
-    # Check if the image_id column is of type int
-    assert result['image_id'].dtype == int
-
-    # Check if the number of rows in the result == number of rows in the sample
-    assert result.shape[0] == sample_df.shape[0]
-
-    # Check if the articleType and baseColour columns only contain values
-    # that are in the article_categories and color_names lists
-    article_categories = ['Shirts', 'Jeans',
-                          'Track Pants', 'Tshirts',
-                          'Casual Shoes']
-
-    color_names = ['Navy Blue', 'Blue', 'Silver', 'Black', 'Grey']
-    assert set(result['articleType'].unique()).issubset(article_categories)
-    assert set(result['baseColour'].unique()).issubset(color_names)
