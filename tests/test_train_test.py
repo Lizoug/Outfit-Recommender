44e1e02acbb51fc5dc794002dd34d8534a69c872
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-11 15:33:50 +0100

    clean code

--- tests/test_train_test.py
+++ tests/test_train_test.py
@@ -6,6 +6,7 @@ def test_train_test():
     path_csv = '/Users/lizak/OneDrive/Desktop/myntradataset/styles.csv'
     path_image = "/Users/lizak/OneDrive/Desktop/myntradataset/images/"
     data = create_dataset(path_csv)
+    # Test case 1: Article labels
     images, lable_article = convert_image_to_array_endlist(path_image,
                                                            data, "article")
     X_train, X_test, y_train, y_test = train_test(images,
