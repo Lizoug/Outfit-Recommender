e64efabbcf303adce3be51fa6c9cf89d84695c68
Shazil Khan <73638219+Bro-tec@users.noreply.github.com>
2023-02-14 23:16:04 +0100

    Update save.py
    cleancoded save because it has further purposes

--- code/save.py
+++ code/save.py
@@ -3,6 +3,10 @@ import return_color as color
 import pandas as pd
 
 def save_data(file):
+    """ Returns predicted clothing Item and its color from file.
+        Further purposes are to save the data in a csv file and instead 
+        of a written text, it should return the clothes images from the user """
+    
     clothing = image.predict(file)
     cloth_color = color.get_color(file)
     return clothing, cloth_color
