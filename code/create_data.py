3c05bded9b08c35f6ae27dc88c45a490d51091db
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-15 09:30:27 +0100

    changed '

--- code/create_data.py
+++ code/create_data.py
@@ -11,45 +11,45 @@ def create_dataset(path_csv):
         the categories"""
 
     # create dataframe, include only lines without errors
-    data = pd.read_csv(path_csv, delimiter=",", on_bad_lines='skip')
+    data = pd.read_csv(path_csv, delimiter=",", on_bad_lines="skip")
 
     # Drop features not needed for analysis
-    data = data.drop(['year', 'usage', 'productDisplayName',
-                      'masterCategory', 'subCategory'], axis=1)
+    data = data.drop(["year", "usage", "productDisplayName",
+                      "masterCategory", "subCategory"], axis=1)
 
     # Rename the id column to image_id
-    data = data.rename(columns={'id': 'image_id'})
+    data = data.rename(columns={"id": "image_id"})
 
     # List of article categories
-    article_categories = ['Shirts', 'Jeans', 'Track Pants',
-                          'Tshirts', 'Casual Shoes', 'Tops',
-                          'Sandals', 'Sweatshirts', 'Formal Shoes',
-                          'Waistcoat', 'Sports Shoes', 'Shorts',
-                          'Heels', 'Innerwear Vests', 'Rain Jacket',
-                          'Dresses', 'Skirts', 'Blazers',
-                          'Shrug', 'Trousers', 'Jackets', 'Sports Sandals',
-                          'Sweaters', 'Tracksuits',
-                          'Leggings', 'Jumpsuit', 'Robe',
-                          'Salwar and Dupatta', 'Kurtas', 'Sarees']
+    article_categories = ["Shirts", "Jeans", "Track Pants",
+                          "Tshirts", "Casual Shoes", "Tops",
+                          "Sandals", "Sweatshirts", "Formal Shoes",
+                          "Waistcoat", "Sports Shoes", "Shorts",
+                          "Heels", "Innerwear Vests", "Rain Jacket",
+                          "Dresses", "Skirts", "Blazers",
+                          "Shrug", "Trousers", "Jackets", "Sports Sandals",
+                          "Sweaters", "Tracksuits",
+                          "Leggings", "Jumpsuit", "Robe",
+                          "Salwar and Dupatta", "Kurtas", "Sarees"]
     # List of colors
-    # For future color prediction purposes. At the moment, we are only using 
+    # For future color prediction purposes. At the moment, we are only using
     # article categories (clothing types) in our modell
     # For color prediction, we are using the color entries of the csv data at the moment"""
-    color_names = ['Navy Blue', 'Blue', 'Silver',
-                   'Black', 'Grey', 'Green',
-                   'Purple', 'White', 'Beige',
-                   'Brown', 'Bronze', 'Teal',
-                   'Copper', 'Pink', 'Off White',
-                   'Maroon', 'Red', 'Khaki',
-                   'Orange', 'Coffee Brown',
-                   'Yellow', 'Charcoal', 'Gold',
-                   'Steel', 'Tan', 'Multi', 'Magenta',
-                   'Lavender', 'Sea Green', 'Cream',
-                   'Peach', 'Olive', 'Skin', 'Burgundy',
-                   'Grey Melange', 'Rust', 'Rose',
-                   'Lime Green', 'Mauve', 'Turquoise Blue',
-                   'Metallic', 'Mustard', 'Taupe',
-                   'Nude', 'Mushroom Brown', 'Fluorescent Green']
+    color_names = ["Navy Blue", "Blue", "Silver",
+                   "Black", "Grey", "Green",
+                   "Purple", "White", "Beige",
+                   "Brown", "Bronze", "Teal",
+                   "Copper", "Pink", "Off White",
+                   "Maroon", "Red", "Khaki",
+                   "Orange", "Coffee Brown",
+                   "Yellow", "Charcoal", "Gold",
+                   "Steel", "Tan", "Multi", "Magenta",
+                   "Lavender", "Sea Green", "Cream",
+                   "Peach", "Olive", "Skin", "Burgundy",
+                   "Grey Melange", "Rust", "Rose",
+                   "Lime Green", "Mauve", "Turquoise Blue",
+                   "Metallic", "Mustard", "Taupe",
+                   "Nude", "Mushroom Brown", "Fluorescent Green"]
 
     # Filter the data to only include entries where
     # articleType is in article_categories
@@ -73,11 +73,11 @@ def train_test(path_image, label_data, label_name):
     if label_name == "article":
         # Read in the article labels and perform one-hot encoding
         df = pd.DataFrame({"label": label_data})
-        one_hot_labels = pd.get_dummies(df['label'])
+        one_hot_labels = pd.get_dummies(df["label"])
     elif label_name == "color":
         # Read in the color labels and perform one-hot encoding
         df = pd.DataFrame({"label": label_data})
-        one_hot_labels = pd.get_dummies(df['label'])
+        one_hot_labels = pd.get_dummies(df["label"])
 
     # Convert the one hot encoded labels to numpy arrays
     labels_np = one_hot_labels.to_numpy()
@@ -98,20 +98,20 @@ lable_list = []
 
 
 def convert_image_to_array_endlist(path_image, data, label_name):
-    """ This function takes the arguments 'path_image' and data.
+    """ This function takes the arguments "path_image" and data.
         Within the function, it uses the glob library to get
         all the jpg images in the given path, enumerates through them,
         extracts the image id from the filename, uses that id to find
         the corresponding entry in a dataframe, opens the image using
         the Image module and converts it to a numpy array. If the image
         shape is (384,256,3) then it appends the image and the corresponding
-        article type to the 'images' and 'lable_article' lists respectively.
-        It returns the 'images' and 'lable_article' lists."""
+        article type to the "images" and "lable_article" lists respectively.
+        It returns the "images" and "lable_article" lists."""
 
-    for i, filename in enumerate(glob.glob(f'{path_image}*.jpg')):
+    for i, filename in enumerate(glob.glob(f"{path_image}*.jpg")):
 
         # Extract the image id from the filename
-        image_id = int(filename.split('\\')[-1].split('.')[0])
+        image_id = int(filename.split("\\")[-1].split(".")[0])
 
         # Use the image id to find the corresponding entry dataframe
         df_entry_with_maching_image_id = data[data["image_id"] == image_id]
@@ -137,9 +137,9 @@ def convert_image_to_array_endlist(path_image, data, label_name):
 
 
 # runs all of the functions above
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = os.getcwd()
-    path_csv = os.path.join(root, 'styles.csv')
+    path_csv = os.path.join(root, "styles.csv")
     # path_csv = "/Users/lizak/OneDrive/Desktop/myntradataset"
     path_image = "/Users/lizak/OneDrive/Desktop/myntradataset/images/"
     data = create_dataset(path_csv)
