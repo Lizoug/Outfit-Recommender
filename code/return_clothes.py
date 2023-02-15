b885efb3dd9652eadfd8b4afd3eba62d32d6bdc0
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-15 09:16:45 +0100

    return_clothes.py pycodestyle

--- code/return_clothes.py
+++ code/return_clothes.py
@@ -1,26 +1,40 @@
-import pandas as pd
 import random
 
 # Categorize the clothing items
-jackets = ['Waistcoat', 'Rain Jacket', 'Blazers', 'Shrug', 'Jackets', 'Kurtas', 'Sarees']
+jackets = ["Waistcoat", "Rain Jacket",
+           "Blazers", "Shrug", "Jackets",
+           "Kurtas", "Sarees"]
 
-top = ['Shirts', 'Tshirts', 'Tops', 'Sweatshirts', 'Innerwear Vests', 'Sweaters', 
-       'Tracksuits', 'Jumpsuit', 'Robe', 'Salwar and Dupatta']
+top = ["Shirts", "Tshirts", "Tops",
+       "Sweatshirts", "Innerwear Vests",
+       "Sweaters", "Tracksuits", "Jumpsuit",
+       "Robe", "Salwar and Dupatta"]
 
-bottom = ['Jeans', 'Track Pants', 'Dresses', 'Skirts', 'Trousers', 'Leggings']
+bottom = ["Jeans", "Track Pants",
+          "Dresses", "Skirts",
+          "Trousers", "Leggings"]
 
-shoes = ['Casual Shoes', 'Sandals', 'Formal Shoes', 'Sports Shoes', 'Heels']
+shoes = ["Casual Shoes", "Sandals",
+         "Formal Shoes", "Sports Shoes",
+         "Heels"]
 
-colors =  ["White", "Black", "Red", "Green", "Blue", "Yellow", "Magenta", "Cyan",
-           "Maroon", "Olive", "Navy", "Yellow Green", "Purple", "Teal", "Orange",
-           "Violet", "Mint Green", "Lime Green", "Pink", "Light Blue", "Gray", 
-           "Light Gray", "Dark Gray", "Lavender", "Mustard", "Seafoam", "Olive Drab",]
+colors = ["White", "Black", "Red",
+          "Green", "Blue", "Yellow",
+          "Magenta", "Cyan", "Maroon",
+          "Olive", "Navy", "Yellow Green",
+          "Purple", "Teal", "Orange",
+          "Violet", "Mint Green", "Lime Green",
+          "Pink", "Light Blue", "Gray",
+          "Light Gray", "Dark Gray",
+          "Lavender", "Mustard", "Seafoam",
+          "Olive Drab"]
+
 
 def clothing_item_is_in(clothes_list, clothing_item):
     """ Checks if clothing list already contains the clothing item"""
-    # Get the first layer of clothes_list 
+    # Get the first layer of clothes_list
     for first_layer_clothes_list in clothes_list:
-        # Get the second layer of clothes_list 
+        # Get the second layer of clothes_list
         for second_layer_clothes_list in first_layer_clothes_list:
             # Get each value of clothing item
             for clothing_value in clothing_item:
@@ -39,11 +53,12 @@ def color_in_clothes(clothes_list, color):
 
 
 def get_clothes(cloth, color):
-    """ Returns a set of clothes with colors, matching with the clothing item of the user"""
-    clothes_list = [[cloth,color]]
+    """ Returns a set of clothes with colors,
+        matching with the clothing item of the user"""
+    clothes_list = [[cloth, color]]
     # Execute three times for three clothing items
     for i in range(3):
-        # Blue is just our placeholder 
+        # Blue is just our placeholder
         col = "Blue"
         actual_list = []
         # Checks if clothing item is in chothes list and chooses it
@@ -55,12 +70,12 @@ def get_clothes(cloth, color):
             actual_list = bottom
         else:
             actual_list = shoes
-        
+
         # Setting a random clothing item from choosed category
-        random_cloth = actual_list[random.randint(0,len(actual_list)-1)]
+        random_cloth = actual_list[random.randint(0, len(actual_list)-1)]
         # Continues throwing out random color until the color is not already in clothes
         while color_in_clothes(clothes_list, col):
-            col = colors[random.randint(0,len(colors)-1)]
+            col = colors[random.randint(0, len(colors)-1)]
         # Add clothes item and its color to clothes_list
         clothes_list.append([random_cloth, col])
     return clothes_list
