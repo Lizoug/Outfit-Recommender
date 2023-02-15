799cf384dd11138f404608b47d9b9a9cfebfcb79
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-15 09:11:38 +0100

    predict_image.py pycodestyle

--- code/predict_image.py
+++ code/predict_image.py
@@ -1,18 +1,17 @@
 from tensorflow import keras
-import numpy as np 
-import os
 from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
+
 
 def predict(file):
     """ Prediction of the clothing items of the images."""
 
     # Loading the model from keras
     model = keras.models.load_model('../model/Trained_for_Outfits.h5')
-    
+
     # Set the sizes for our prediction
     size = 256, 384
-    
+
     # Resize the image
     image = Image.open(file)
     # Resize the image
@@ -21,10 +20,18 @@ def predict(file):
     img = np.array(new_image)
     # Setting the array to the shape of our model
     i = img[None, :]
-    
+
     # Return predictions for image
     p = model.predict(i)
-    
-    class_names = ['Blazers', 'Casual Shoes', 'Dresses', 'Formal Shoes', 'Heels', 'Innerwear Vests', 'Jackets', 'Jeans', 'Jumpsuit', 'Kurtas', 'Leggings', 'Rain Jacket', 'Robe', 'Salwar and Dupatta', 'Sandals', 'Sarees', 'Shirts', 'Shorts', 'Shrug', 'Skirts', 'Sports Sandals', 'Sports Shoes', 'Sweaters', 'Sweatshirts', 'Tops', 'Track Pants', 'Tracksuits', 'Trousers', 'Tshirts', 'Waistcoat']
+
+    class_names = ['Blazers', 'Casual Shoes', 'Dresses',
+                   'Formal Shoes', 'Heels', 'Innerwear Vests',
+                   'Jackets', 'Jeans', 'Jumpsuit', 'Kurtas',
+                   'Leggings', 'Rain Jacket', 'Robe',
+                   'Salwar and Dupatta', 'Sandals', 'Sarees',
+                   'Shirts', 'Shorts', 'Shrug', 'Skirts',
+                   'Sports Sandals', 'Sports Shoes', 'Sweaters',
+                   'Sweatshirts', 'Tops', 'Track Pants', 'Tracksuits',
+                   'Trousers', 'Tshirts', 'Waistcoat']
     # Returns the value which was predicted with class_names
     return class_names[np.argmax(p[0])]
